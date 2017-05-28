13f63dc593dfc217207f18d51af1948c37ffcc80
Zanqi Liang <zanqi.liang@gmail.com>
2017-05-28 20:20:05 +0800

    First commit

---
+++ Ch1/Final/AutomatedTellerMachine/MyLoggingFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine
+{
+    public class MyLoggingFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var request = filterContext.HttpContext.Request;
+            Logger.LogRequest(request.UserHostAddress);
+            filterContext.HttpContext.Response.AppendHeader("UserHostAddress", request.UserHostAddress);
+            base.OnActionExecuted(filterContext);
+        }
+
+    }
+
+    internal class Logger
+    {
+        internal static void LogRequest(string userHostAddress)
+        {
+            Console.WriteLine($"User Performed an Action: {userHostAddress}");
+        }
+    }
+}
---
+++ Ch9/Start/AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -0,0 +1,90 @@
+﻿using AutomatedTellerMachine.Models;
+using AutomatedTellerMachine.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    [Authorize]
+    public class TransactionController : Controller
+    {
+        private IApplicationDbContext db;
+
+        public TransactionController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public TransactionController(IApplicationDbContext dbContext)
+        {
+            db = dbContext;
+        }
+
+        public ActionResult Deposit(int checkingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Deposit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(transaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult QuickCash(int checkingAccountId, decimal amount)
+        {
+            var sourceCheckingAccount = db.CheckingAccounts.Find(checkingAccountId);
+            var balance = sourceCheckingAccount.Balance;
+            if (balance < amount)
+            {
+                return View("QuickCashInsufficientFunds");
+            }
+            db.Transactions.Add(new Transaction { CheckingAccountId = checkingAccountId, Amount = -amount });
+            db.SaveChanges();
+
+            var service = new CheckingAccountService(db);
+            service.UpdateBalance(checkingAccountId);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Withdrawal(int checkingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Withdrawal(Transaction transaction)
+        {
+            var checkingAccount = db.CheckingAccounts.Find(transaction.CheckingAccountId);
+            if (checkingAccount.Balance < transaction.Amount)
+            {
+                ModelState.AddModelError("Amount", "You have insufficient funds!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                transaction.Amount = -transaction.Amount;
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(transaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
